8e0709a0b88e2ecb6fdcf6d3c8e7774a27462eb1
Indra Kurniawan <kurniawanindra9d@gmail.com>
2025-04-27 00:55:40 +0700

    feat: add delete modal for manage  user & manage product

---
+++ src/components/modal/manage-user/ModalDeleteUser.jsx
@@ -0,0 +1,90 @@
+import {
+  Button,
+  Dialog,
+  Portal,
+  Text,
+  VStack,
+  HStack,
+  Flex,
+} from "@chakra-ui/react";
+import { WarningCircle } from "@phosphor-icons/react";
+
+const ModalDeleteUser = ({ isOpen, onClose }) => {
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={(open) => !open && onClose()}>
+      <Portal>
+        <Dialog.Backdrop backdropFilter="blur(8px)" bg="rgba(0, 0, 0, 0.4)" />
+        <Dialog.Positioner
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minH="100vh"
+        >
+          <Dialog.Content
+            borderRadius="2xl"
+            bg="white"
+            w="md"
+            p={{ base: 4, lg: 6 }}
+            gap={{ base: 4, lg: 6 }}
+            boxShadow="none"
+          >
+            <Dialog.Body as={VStack} align="stretch" gap={6} padding={0}>
+              <WarningCircle size={80} color="red" />
+              <Text
+                fontSize="24px"
+                textAlign={"center"}
+                fontWeight="medium"
+                color="black"
+                lineHeight={1.2}
+              >
+                Anda yakin ingin menghapus Pengguna ini?
+              </Text>
+              <Text
+                fontSize="16px"
+                textAlign={"center"}
+                fontWeight="semibold"
+                color="#828282"
+                lineHeight={1.2}
+              >
+                Data yang dihapus tidak dapat dipulihkan
+              </Text>
+              <Flex w="full" justifyContent="center">
+                <HStack spacing={3}>
+                  <Button
+                    size={"sm"}
+                    bg={"#828282"}
+                    color={"white"}
+                    rounded={"xl"}
+                    px={5}
+                    py={4}
+                    _hover={{ bg: "gray.500", color: "white" }}
+                    onClick={onClose}
+                  >
+                    <Text lineHeight="1" whiteSpace="nowrap">
+                      Batal
+                    </Text>
+                  </Button>
+                  <Button
+                    size={"sm"}
+                    bg={"red.500"}
+                    color={"white"}
+                    rounded={"xl"}
+                    px={5}
+                    py={4}
+                    _hover={{ bg: "red.600" }}
+                  >
+                    <Text lineHeight="1" whiteSpace="nowrap">
+                      Hapus
+                    </Text>
+                  </Button>
+                </HStack>
+              </Flex>
+            </Dialog.Body>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  );
+};
+
+export default ModalDeleteUser;
--- src/components/tables/manage-user/TableUserList.jsx
+++ src/components/tables/manage-user/TableUserList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IconButton } from "@chakra-ui/react";
 import { BaseTable } from "../base-table/BaseTable";
 import {
@@ -5,8 +6,9 @@ import {
   TextCell,
   LeftAlignCell,
 } from "../base-table/TableCells";
-import { Eye, Trash } from "iconsax-react";
+import { Trash } from "iconsax-react";
 import { TableBodyRow } from "../base-table/TableRows";
+import ModalDeleteUser from "../../modal/manage-user/ModalDeleteUser";
 
 const TABLEHEADS = ["No", "Nama Lengkap", "Email", "Nomor Telepon", "Aksi"];
 
@@ -19,23 +21,36 @@ export function TableUserList({
   const endIndex = startIndex + itemsPerPage;
   const displayedUsers = users.slice(startIndex, endIndex);
 
+  const [isOpenDelete, setIsOpenDelete] = useState(false);
+
   return (
-    <BaseTable data={displayedUsers} heads={TABLEHEADS}>
-      {displayedUsers.map((user, index) => (
-        <TableBodyRow key={user.id} index={index}>
-          <CenteredCell>
-            {(currentPage - 1) * itemsPerPage + index + 1}
-          </CenteredCell>
-          <TextCell content={user.fullName} />
-          <TextCell content={user.email} />
-          <LeftAlignCell>{user.phone}</LeftAlignCell>
-          <CenteredCell>
-            <IconButton variant="ghost" _hover={{ bg: "transparent" }} ml={2}>
-              <Trash color="red" />
-            </IconButton>
-          </CenteredCell>
-        </TableBodyRow>
-      ))}
-    </BaseTable>
+    <>
+      <BaseTable data={displayedUsers} heads={TABLEHEADS}>
+        {displayedUsers.map((user, index) => (
+          <TableBodyRow key={user.id} index={index}>
+            <CenteredCell>
+              {(currentPage - 1) * itemsPerPage + index + 1}
+            </CenteredCell>
+            <TextCell content={user.fullName} />
+            <TextCell content={user.email} />
+            <LeftAlignCell>{user.phone}</LeftAlignCell>
+            <CenteredCell>
+              <IconButton
+                variant="ghost"
+                _hover={{ bg: "transparent" }}
+                ml={2}
+                onClick={() => setIsOpenDelete(true)}
+              >
+                <Trash color="red" />
+              </IconButton>
+            </CenteredCell>
+          </TableBodyRow>
+        ))}
+      </BaseTable>
+      <ModalDeleteUser
+        isOpen={isOpenDelete}
+        onClose={() => setIsOpenDelete(false)}
+      />
+    </>
   );
 }
