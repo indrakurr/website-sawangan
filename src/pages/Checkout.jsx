2856a162968efc6d48c9202c53b337ced13f24ba
Indra Kurniawan <kurniawanindra9d@gmail.com>
2025-05-09 21:45:07 +0700

    feat: get shipping cost

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -16,16 +16,75 @@ import CheckoutItem from "../components/card/CheckoutItem";
 import { User } from "react-iconly";
 import { PhoneCall, Signpost } from "@phosphor-icons/react";
 import { useLocation } from "react-router-dom";
-import { useMemo } from "react";
+import { useMemo, useState, useEffect } from "react";
 
 export default function Checkout() {
   const { state: selectedItems = [] } = useLocation();
 
+  const [postalCode, setPostalCode] = useState("");
+  const [destinationId, setDestinationId] = useState("");
+  const [shippingCost, setShippingCost] = useState(0);
+
   const subtotal = useMemo(() => {
     return selectedItems.reduce((sum, item) => {
-      return sum + item.product.price * item.quantity;
+      const price = item?.product?.price ?? 0;
+      const qty = item?.quantity ?? 0;
+      return sum + price * qty;
     }, 0);
   }, [selectedItems]);
+
+  const totalWeight = useMemo(() => {
+    return selectedItems.reduce((sum, item) => {
+      const weight = item.product?.weight ?? 0;
+      const qty = item.quantity ?? 0;
+      return sum + weight * qty;
+    }, 0);
+  }, [selectedItems]);
+
+  useEffect(() => {
+    const fetchShipping = async () => {
+      try {
+        if (!postalCode || postalCode.length < 5) return;
+
+        const resDest = await fetch(
+          `${
+            import.meta.env.VITE_API_BASE_URL
+          }/shipping/destinations?keyword=${postalCode}`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        const destData = await resDest.json();
+        const destId = destData?.data?.[0]?.id;
+        if (!destId) return;
+        setDestinationId(destId);
+
+        const resShip = await fetch(
+          `${
+            import.meta.env.VITE_API_BASE_URL
+          }/shipping/options?shipper_destination_id=72910&receiver_destination_id=${destId}&weight=${totalWeight}&item_value=${subtotal}&cod=no`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+
+        const shipData = await resShip.json();
+        const jne = shipData?.data?.find((s) => s.shipping_name === "JNE");
+
+        if (jne) {
+          setShippingCost(jne.shipping_cost);
+        }
+      } catch (err) {
+        console.error("Gagal fetch ongkir:", err);
+      }
+    };
+
+    fetchShipping();
+  }, [postalCode, totalWeight, subtotal]);
 
   return (
     <div
@@ -61,11 +120,10 @@ export default function Checkout() {
                 fontSize={{ base: "16px", lg: "20px" }}
                 fontWeight="semibold"
                 color="black"
-                lineHeight={1}
               >
                 Detail Pengiriman
               </Text>
-              <VStack align="start" gap={4} w="full" marginTop={"24px"}>
+              <VStack align="start" gap={4} w="full" marginTop="24px">
                 <InputWithLogo
                   id="nama-penerima"
                   label="Nama Penerima"
@@ -73,7 +131,7 @@ export default function Checkout() {
                   icon={User}
                 />
                 <InputWithLogo
-                  id="nama-penerima"
+                  id="nomor-telepon"
                   label="Nomor Telepon"
                   type="tel"
                   icon={PhoneCall}
@@ -91,10 +149,13 @@ export default function Checkout() {
                   label="Kode Pos"
                   type="tel"
                   icon={Signpost}
+                  value={postalCode}
+                  onChange={(e) => setPostalCode(e.target.value)}
                 />
               </VStack>
             </Box>
           </GridItem>
+
           <GridItem colSpan={1} position="sticky">
             <Box
               bg="white"
@@ -106,11 +167,10 @@ export default function Checkout() {
                 fontSize={{ base: "16px", lg: "20px" }}
                 fontWeight="semibold"
                 color="black"
-                lineHeight={1}
               >
                 Ringkasan Belanja
               </Text>
-              <Box flex="1" height="1px" bg="gray.200" marginTop={"24px"} />
+              <Box flex="1" height="1px" bg="gray.200" marginTop="24px" />
               <VStack w="full" marginTop="8px">
                 {selectedItems.map((item) => (
                   <CheckoutItem
@@ -120,74 +180,50 @@ export default function Checkout() {
                   />
                 ))}
               </VStack>
-              <Flex justifyContent="space-between" marginTop={"24px"}>
-                <Text
-                  textAlign="start"
-                  fontSize="14px"
-                  fontWeight="semibold"
-                  color="gray.400"
-                  lineHeight={"1.2"}
-                >
+              <Flex justifyContent="space-between" marginTop="24px">
+                <Text fontSize="14px" fontWeight="semibold" color="gray.400">
                   Subtotal
                 </Text>
                 <Text
-                  textAlign="end"
                   fontSize={{ base: "12px", lg: "16px" }}
                   fontWeight="bold"
                   color="black"
-                  lineHeight={"1.2"}
                 >
                   Rp {subtotal.toLocaleString("id-ID")}
                 </Text>
               </Flex>
-              <Flex justifyContent="space-between" marginTop={"12px"}>
-                <Text
-                  textAlign="start"
-                  fontSize="14px"
-                  fontWeight="semibold"
-                  color="gray.400"
-                  lineHeight={"1.2"}
-                >
+              <Flex justifyContent="space-between" marginTop="12px">
+                <Text fontSize="14px" fontWeight="semibold" color="gray.400">
                   Pengiriman
                 </Text>
                 <Text
-                  textAlign="end"
                   fontSize={{ base: "12px", lg: "16px" }}
                   fontWeight="bold"
                   color="black"
-                  lineHeight={"1.2"}
                 >
-                  Rp 450.000
+                  Rp {shippingCost.toLocaleString("id-ID")}
                 </Text>
               </Flex>
-              <Box flex="1" height="1px" bg="gray.200" marginTop={"24px"} />
-              <Flex justifyContent="space-between" marginTop={"24px"}>
-                <Text
-                  textAlign="start"
-                  fontSize="14px"
-                  fontWeight="semibold"
-                  color="gray.400"
-                  lineHeight={"1.2"}
-                >
+              <Box flex="1" height="1px" bg="gray.200" marginTop="24px" />
+              <Flex justifyContent="space-between" marginTop="24px">
+                <Text fontSize="14px" fontWeight="semibold" color="gray.400">
                   Total Tagihan
                 </Text>
                 <Text
-                  textAlign="end"
                   fontSize={{ base: "16px", lg: "20px" }}
                   fontWeight="bold"
                   color="orange.500"
-                  lineHeight={"1.2"}
                 >
-                  Rp 450.000
+                  Rp {(subtotal + shippingCost).toLocaleString("id-ID")}
                 </Text>
               </Flex>
               <Button
-                marginTop={"24px"}
-                size={"sm"}
-                bg={"orange.500"}
-                color={"white"}
-                rounded={"xl"}
-                w={"full"}
+                marginTop="24px"
+                size="sm"
+                bg="orange.500"
+                color="white"
+                rounded="xl"
+                w="full"
                 py={5}
                 _hover={{ bg: "orange.600" }}
               >
