4bf278a2fd6febf1571e6db0ac028ae0fc189c14
Indra Kurniawan <kurniawanindra9d@gmail.com>
2025-05-13 04:37:58 +0700

    feat: modified toaster

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -122,6 +122,7 @@ export default function Checkout() {
             description: "Kamu akan diarahkan ke halaman pembayaran.",
           });
           window.open(paymentUrl, "_blank");
+          window.location.href = "/orders";
         } else {
           toaster.error({
             title: "URL Pembayaran Tidak Ditemukan",
@@ -144,163 +145,171 @@ export default function Checkout() {
   };
 
   return (
-    <div
-      className="overflow-x-hidden w-full max-w-screen mx-0 bg-[#F0F3F7]"
-      style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
-    >
-      <Navbar />
-      <Container paddingX={{ base: "0px", lg: "80px" }}>
-        <Text
-          marginTop={{ base: "72px", lg: "89px" }}
-          fontSize={{ base: "24px", lg: "32px" }}
-          fontWeight="bold"
-          color="black"
-          paddingX={{ base: "16px", lg: "32px" }}
-        >
-          Checkout
-        </Text>
-        <Grid
-          className="grid lg:grid-cols-2 sm:grid-rows-1"
-          marginTop={{ base: "16px", lg: "24px" }}
-          marginBottom={{ base: "24px", lg: "52px" }}
-          paddingX={{ base: "12px", lg: "32px" }}
-          gap={{ base: "12px", lg: "24px" }}
-        >
-          <GridItem colSpan={1} className="flex flex-col">
-            <Box
-              bg="white"
-              borderRadius="xl"
-              padding={6}
-              boxShadow="0px 4px 30px rgba(0, 0, 0, 0.1)"
-            >
-              <Text
-                fontSize={{ base: "16px", lg: "20px" }}
-                fontWeight="semibold"
-                color="black"
+    <>
+      <div
+        className="overflow-x-hidden w-full max-w-screen mx-0 bg-[#F0F3F7]"
+        style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+      >
+        <Navbar />
+        <Container paddingX={{ base: "0px", lg: "80px" }}>
+          <Text
+            marginTop={{ base: "72px", lg: "89px" }}
+            fontSize={{ base: "24px", lg: "32px" }}
+            fontWeight="bold"
+            color="black"
+            paddingX={{ base: "16px", lg: "32px" }}
+          >
+            Checkout
+          </Text>
+          <Grid
+            className="grid lg:grid-cols-2 sm:grid-rows-1"
+            marginTop={{ base: "16px", lg: "24px" }}
+            marginBottom={{ base: "24px", lg: "52px" }}
+            paddingX={{ base: "12px", lg: "32px" }}
+            gap={{ base: "12px", lg: "24px" }}
+          >
+            <GridItem colSpan={1} className="flex flex-col">
+              <Box
+                bg="white"
+                borderRadius="xl"
+                padding={6}
+                boxShadow="0px 4px 30px rgba(0, 0, 0, 0.1)"
               >
-                Detail Pengiriman
-              </Text>
-              <VStack align="start" gap={4} w="full" marginTop="24px">
-                <InputWithLogo
-                  id="nama-penerima"
-                  label="Nama Penerima"
-                  type="text"
-                  icon={User}
-                />
-                <InputWithLogo
-                  id="nomor-telepon"
-                  label="Nomor Telepon"
-                  type="tel"
-                  icon={PhoneCall}
-                />
-                <Textarea
-                  id="alamat-lengkap"
-                  placeholder="Alamat Penerima"
-                  color="black"
-                  fontSize={{ base: "12px", lg: "16px" }}
-                  borderColor="gray.300"
-                  height="96px"
-                />
-                <InputWithLogo id="kota" label="Kota" type="text" icon={City} />
-                <InputWithLogo
-                  id="provinsi"
-                  label="Provinsi"
-                  type="text"
-                  icon={MapTrifold}
-                />
-                <InputWithLogo
-                  id="kode-pos"
-                  label="Kode Pos"
-                  type="tel"
-                  icon={Signpost}
-                  value={postalCode}
-                  onChange={(e) => setPostalCode(e.target.value)}
-                />
-              </VStack>
-            </Box>
-          </GridItem>
-
-          <GridItem colSpan={1} position="sticky">
-            <Box
-              bg="white"
-              borderRadius="xl"
-              padding={6}
-              boxShadow="0px 4px 30px rgba(0, 0, 0, 0.1)"
-            >
-              <Text
-                fontSize={{ base: "16px", lg: "20px" }}
-                fontWeight="semibold"
-                color="black"
-              >
-                Ringkasan Belanja
-              </Text>
-              <Box flex="1" height="1px" bg="gray.200" marginTop="24px" />
-              <VStack w="full" marginTop="8px">
-                {selectedItems.map((item) => (
-                  <CheckoutItem
-                    key={item.productId}
-                    product={item.product}
-                    quantity={item.quantity}
-                  />
-                ))}
-              </VStack>
-              <Flex justifyContent="space-between" marginTop="24px">
-                <Text fontSize="14px" fontWeight="semibold" color="gray.400">
-                  Subtotal
-                </Text>
-                <Text
-                  fontSize={{ base: "12px", lg: "16px" }}
-                  fontWeight="bold"
-                  color="black"
-                >
-                  Rp {subtotal.toLocaleString("id-ID")}
-                </Text>
-              </Flex>
-              <Flex justifyContent="space-between" marginTop="12px">
-                <Text fontSize="14px" fontWeight="semibold" color="gray.400">
-                  Pengiriman
-                </Text>
-                <Text
-                  fontSize={{ base: "12px", lg: "16px" }}
-                  fontWeight="bold"
-                  color="black"
-                >
-                  Rp {shippingCost.toLocaleString("id-ID")}
-                </Text>
-              </Flex>
-              <Box flex="1" height="1px" bg="gray.200" marginTop="24px" />
-              <Flex justifyContent="space-between" marginTop="24px">
-                <Text fontSize="14px" fontWeight="semibold" color="gray.400">
-                  Total Tagihan
-                </Text>
                 <Text
                   fontSize={{ base: "16px", lg: "20px" }}
-                  fontWeight="bold"
-                  color="orange.500"
+                  fontWeight="semibold"
+                  color="black"
                 >
-                  Rp {(subtotal + shippingCost).toLocaleString("id-ID")}
+                  Detail Pengiriman
                 </Text>
-              </Flex>
-              <Button
-                marginTop="24px"
-                size="sm"
-                bg="orange.500"
-                color="white"
-                rounded="xl"
-                w="full"
-                py={5}
-                _hover={{ bg: "orange.600" }}
-                onClick={handleCheckout}
+                <VStack align="start" gap={4} w="full" marginTop="24px">
+                  <InputWithLogo
+                    id="nama-penerima"
+                    label="Nama Penerima"
+                    type="text"
+                    icon={User}
+                  />
+                  <InputWithLogo
+                    id="nomor-telepon"
+                    label="Nomor Telepon"
+                    type="tel"
+                    icon={PhoneCall}
+                  />
+                  <Textarea
+                    id="alamat-lengkap"
+                    placeholder="Alamat Penerima"
+                    color="black"
+                    fontSize={{ base: "12px", lg: "16px" }}
+                    borderColor="gray.300"
+                    height="96px"
+                  />
+                  <InputWithLogo
+                    id="kota"
+                    label="Kota"
+                    type="text"
+                    icon={City}
+                  />
+                  <InputWithLogo
+                    id="provinsi"
+                    label="Provinsi"
+                    type="text"
+                    icon={MapTrifold}
+                  />
+                  <InputWithLogo
+                    id="kode-pos"
+                    label="Kode Pos"
+                    type="tel"
+                    icon={Signpost}
+                    value={postalCode}
+                    onChange={(e) => setPostalCode(e.target.value)}
+                  />
+                </VStack>
+              </Box>
+            </GridItem>
+
+            <GridItem colSpan={1} position="sticky">
+              <Box
+                bg="white"
+                borderRadius="xl"
+                padding={6}
+                boxShadow="0px 4px 30px rgba(0, 0, 0, 0.1)"
               >
-                <Text lineHeight="1" whiteSpace="nowrap">
-                  Buat Pesanan
+                <Text
+                  fontSize={{ base: "16px", lg: "20px" }}
+                  fontWeight="semibold"
+                  color="black"
+                >
+                  Ringkasan Belanja
                 </Text>
-              </Button>
-            </Box>
-          </GridItem>
-        </Grid>
-      </Container>
-      <Footer display={{ base: "none", lg: "block" }} />
-    </div>
+                <Box flex="1" height="1px" bg="gray.200" marginTop="24px" />
+                <VStack w="full" marginTop="8px">
+                  {selectedItems.map((item) => (
+                    <CheckoutItem
+                      key={item.productId}
+                      product={item.product}
+                      quantity={item.quantity}
+                    />
+                  ))}
+                </VStack>
+                <Flex justifyContent="space-between" marginTop="24px">
+                  <Text fontSize="14px" fontWeight="semibold" color="gray.400">
+                    Subtotal
+                  </Text>
+                  <Text
+                    fontSize={{ base: "12px", lg: "16px" }}
+                    fontWeight="bold"
+                    color="black"
+                  >
+                    Rp {subtotal.toLocaleString("id-ID")}
+                  </Text>
+                </Flex>
+                <Flex justifyContent="space-between" marginTop="12px">
+                  <Text fontSize="14px" fontWeight="semibold" color="gray.400">
+                    Pengiriman
+                  </Text>
+                  <Text
+                    fontSize={{ base: "12px", lg: "16px" }}
+                    fontWeight="bold"
+                    color="black"
+                  >
+                    Rp {shippingCost.toLocaleString("id-ID")}
+                  </Text>
+                </Flex>
+                <Box flex="1" height="1px" bg="gray.200" marginTop="24px" />
+                <Flex justifyContent="space-between" marginTop="24px">
+                  <Text fontSize="14px" fontWeight="semibold" color="gray.400">
+                    Total Tagihan
+                  </Text>
+                  <Text
+                    fontSize={{ base: "16px", lg: "20px" }}
+                    fontWeight="bold"
+                    color="orange.500"
+                  >
+                    Rp {(subtotal + shippingCost).toLocaleString("id-ID")}
+                  </Text>
+                </Flex>
+                <Button
+                  marginTop="24px"
+                  size="sm"
+                  bg="orange.500"
+                  color="white"
+                  rounded="xl"
+                  w="full"
+                  py={5}
+                  _hover={{ bg: "orange.600" }}
+                  onClick={handleCheckout}
+                >
+                  <Text lineHeight="1" whiteSpace="nowrap">
+                    Buat Pesanan
+                  </Text>
+                </Button>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Container>
+        <Footer display={{ base: "none", lg: "block" }} />
+      </div>
+      <Toaster/>
+    </>
   );
 }
