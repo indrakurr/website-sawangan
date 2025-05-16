d61d4dd901653117f1b0e66f9a226d215b5c0c7a
Indra Kurniawan <kurniawanindra9d@gmail.com>
2025-05-16 12:29:53 +0700

    fix: protectedroute and adminroute

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,11 +1,41 @@
-// ProtectedRoute.jsx
+import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import axios from "axios";
 
 const ProtectedRoute = () => {
-  const token = localStorage.getItem("token");
+  const [checking, setChecking] = useState(true);
+  const [isValid, setIsValid] = useState(false);
 
-  if (!token) return <Navigate to="/login" replace />;
-  
+  useEffect(() => {
+    const validateToken = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        setIsValid(false);
+        setChecking(false);
+        return;
+      }
+
+      try {
+        await axios.get(`${import.meta.env.VITE_API_BASE_URL}/profile`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setIsValid(true);
+      } catch (err) {
+        localStorage.removeItem("token");
+        setIsValid(false);
+      } finally {
+        setChecking(false);
+      }
+    };
+
+    validateToken();
+  }, []);
+
+  if (checking) return null;
+  if (!isValid) return <Navigate to="/login" replace />;
+
   return <Outlet />;
 };
 
